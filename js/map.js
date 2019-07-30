c342ba94c14c5e52790aa575bf427a72b3148b08
celine <ducruet.celine@gmail.com>
2019-07-30 17:29:15 +0200

    clusters integration

--- js/map.js
+++ js/map.js
@@ -5,43 +5,54 @@ console.log("map");
 class Map {
     init() {
 
-        let mymap = L.map('mymap').setView([45.6963425, 4.735948], 10);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'pk.eyJ1IjoiamF4eDc0IiwiYSI6ImNqeThmNno5bzA4dnIzb29meDhqaXMxZnEifQ.6KLIJuqWcahvJm0G3zCeXA'
-        }).addTo(mymap);
-        
-        
 
 //icone du marker//
         let bikeIcon = L.icon({
-            iconUrl: '../html/pictures/bike_map_marker3.png',
+            iconUrl: '../pictures-/bike_map_marker3.png',
 
 
             iconSize: [50, 50], // size of the icon
             iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
             popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
         });
-        
+
+//Fond de carte//
+
+        let tiles = L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
+            //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoiamF4eDc0IiwiYSI6ImNqeThmNno5bzA4dnIzb29meDhqaXMxZnEifQ.6KLIJuqWcahvJm0G3zCeXA'
+        });
 
 
-//ajout des stations//
+//infos carte et zoom//
 
-        let stationsElt = document.getElementById("stations");
+
+        let latlng = L.latLng(45.757620581029435, 4.8318493366241455);
+        var map = L.map('mymap', {
+            center: latlng,
+            zoom: 13,
+            layers: [tiles]
+        });
+
+
+//ajout des stations et des clusters//
+
+
+        let markers = L.markerClusterGroup()
 
         ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=0cc7beca8ea42259f8a1783a66d0f20c602be69d", function (reponse) {
             let stations = JSON.parse(reponse);
+
             stations.forEach(function (station) {
 
                 let marker = L.marker([station.position.lat, station.position.lng], {
                     icon: bikeIcon
-                }).addTo(mymap);
-
-                new L.marker()
-                console.log(stationsElt)
+                });
+                markers.addLayer(marker);
             });
+            map.addLayer(markers);
         });
     }
 }
--- js/veloV.js
+++ js/veloV.js
@@ -7,3 +7,13 @@ function nextSlide(){
 	currentSlide = (currentSlide+1)%slides.length;
 	slides[currentSlide].className = 'slide one';
 }
+
+//icone du marker//
+let bikeIcon = L.icon({
+	iconUrl: '../html/pictures/bike_map_marker3.png',
+
+
+	iconSize: [50, 50], // size of the icon
+	iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
